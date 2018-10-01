ab3af0b4a6baa1b2f2a1271a3743dfb855046f81
ckern-tgm <ckern@student.tgm.ac.at>
2018-10-01 09:49:00 +0200

    file changed

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('My First Test', function() {
       it('Visits the Kitchen Sink', function() {
-              cy.visit('http://localhost:8081')
+              cy.visit('http://localhost:8080')
             })
     it('clicks link in type function', function() {
         cy.contains('type').click()
